04d81498e543f7616367ea175c6b418f0d945915
Maxime <maxime.duplaissy@gmail.com>
2023-11-25 22:57:13 +0100

    Page d'accueil liste des produits créée

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProductController extends AbstractController
 {
-    #[Route('/', name: 'app_product')]
-    public function index(): Response
-    {
+    #[Route('/', name: 'list_products')]
+	public function index(Request $request, ManagerRegistry $doctrine): Response
+	{
+
+		$em = $doctrine->getManager();
+		$products = $em->getRepository(Product::class)->findAll();
+
+		$session = $request->getSession();
+		$notification = $session->get('notification');
+		$type_notif = $session->get('type_notif');
+
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $products,
+			'notification' => $notification,
+			'type_notif' => $type_notif
         ]);
     }
 }
